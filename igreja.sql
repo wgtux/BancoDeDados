49f2317a9e8e1258c5e5bdc5793635d117240c77
wgtux <weslengomes@gmail.com>
2016-08-29 22:21:50 -0300

    adicionando tabelas

--- igreja.sql
+++ igreja.sql
@@ -3,6 +3,8 @@ Create database igreja
 default character set utf8
 default collate utf8_general_ci;
 
+use igreja;
+
 /*Criando tabelas*/
 create table if not exists membros (
 idMembro int not null auto_increment,
@@ -18,5 +20,29 @@ estado varchar (4),
 tel int (11),
 email varchar (30),
 estadoCivil varchar (10),
-primary key (id)
+primary key (idMembro)
+) default charset=utf8;
+
+create table departamento (
+idDep int not null auto_increment,
+nomeDep varchar (40) not null,
+primary key (idDep)
+) default charset=utf8;
+
+create table cargo (
+idCargo int not null auto_increment,
+nomeCargo varchar (40) not null,
+primary key (idCargo)
+) default charset=utf8;
+
+create table igreja (
+idIgreja int not null auto_increment,
+nomeIgreja varchar (40) not null,
+endIgreja varchar (40) not null,
+bairro varchar (30) not null,
+cep varchar (10),
+cidade varchar (30) not null,
+estado varchar (2),
+pais varchar (20),
+primary key (idIgreja)
 ) default charset=utf8;
