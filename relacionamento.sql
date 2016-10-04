fc444c57512142a0c1216202bdbc0b27a596c045
wgtux <weslengomes@gmail.com>
2016-10-04 17:58:53 -0300

    relacionamento continuação

--- relacionamento.sql
+++ relacionamento.sql
@@ -23,3 +23,21 @@ select * from igreja;
 
 update membros set cargoId = '6' where idMembro = '1';
 update membros set igrejaId = '2' where idMembro = '1';
+
+update membros set cargoId = '5' where idMembro = '2';
+update membros set igrejaId = '3' where idMembro = '2';
+
+update membros set cargoId = '4' where idMembro = '3';
+update membros set igrejaId = '4' where idMembro = '3';
+
+update membros set cargoId = '3' where idMembro = '4';
+update membros set igrejaId = '5' where idMembro = '4';
+
+update membros set cargoId = '2' where idMembro = '5';
+update membros set igrejaId = '1' where idMembro = '5';
+
+update membros set cargoId = '6' where idMembro = '6';
+update membros set igrejaId = '1' where idMembro = '6';
+
+update membros set cargoId = '5' where idMembro = '7';
+update membros set igrejaId = '2' where idMembro = '7';
